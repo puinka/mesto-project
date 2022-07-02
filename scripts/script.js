4dd890bc9dd907c7a6cbaf9bb55f0ba87998a00f
Valeria <vozzheva@mail.ru>
2022-07-02 11:15:58 +0300

    Reorganise js file & add avatar popup const

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,16 @@
-const popupEditProfile = document.querySelector(`.popup_type_edit-profile`);
+//Profile elements
+const profile = document.querySelector(`.profile`);
+const profileUserName = profile.querySelector(`.profile__name`);
+const profileUserOccupation = profile.querySelector(`.profile__occupation`);
+const buttonEditProfile = profile.querySelector(`.profile__edit-button`);
+const buttonAddPlace = profile.querySelector(`.profile__add-button`);
+const buttonEditAvatar = profile.querySelector(`.profile__avatar-edit-button`);
+
+//All close popup buttons
 const popupCloseButtons = document.querySelectorAll(`.popup__close-button`);
+
+//Edit Profile popup elements
+const popupEditProfile = document.querySelector(`.popup_type_edit-profile`);
 const formEditProfile = popupEditProfile.querySelector(
   `.popup__form_type_edit-profile`
 );
@@ -10,6 +21,7 @@ const inputUserOccupation = popupEditProfile.querySelector(
   `.popup__text-input_type_user-occupation`
 );
 
+//Add place popup elements
 const popupAddPlace = document.querySelector(`.popup_type_add-place`);
 const formAddPlace = popupAddPlace.querySelector(`.popup__form_type_add-place`);
 const inputPlaceName = formAddPlace.querySelector(
@@ -19,12 +31,21 @@ const inputPlaceLink = formAddPlace.querySelector(
   `.popup__text-input_type_place-link`
 );
 
-const profile = document.querySelector(`.profile`);
-const profileUserName = profile.querySelector(`.profile__name`);
-const profileUserOccupation = profile.querySelector(`.profile__occupation`);
-const buttonEditProfile = profile.querySelector(`.profile__edit-button`);
-const buttonAddPlace = profile.querySelector(`.profile__add-button`);
+//View photo popup elements
+const viewPhotoPopup = document.querySelector(`.popup_type_view-photo`);
+const popupCaption = viewPhotoPopup.querySelector(`.popup__caption`);
+const popupImage = viewPhotoPopup.querySelector(`.popup__image`);
 
+//Edit avatar popup elements
+const popupEditAvatar = document.querySelector(`.popup_type_edit-avatar`);
+const formEditAvatar = popupEditAvatar.querySelector(
+  `.popup__form_type_edit-avatar`
+);
+const inputAvatarLink = formEditAvatar.querySelector(
+  `.popup__text-input_type_avatar-link`
+);
+
+//Cards
 const elementsSection = document.querySelector(`.elements`);
 
 const initialCards = [
@@ -53,10 +74,6 @@ const initialCards = [
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
   },
 ];
-
-const viewPhotoPopup = document.querySelector(`.popup_type_view-photo`);
-const popupCaption = viewPhotoPopup.querySelector(`.popup__caption`);
-const popupImage = viewPhotoPopup.querySelector(`.popup__image`);
 
 // functions declaration
 
