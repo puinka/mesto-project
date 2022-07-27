f849e7743fd7ce74dbb545720bf10c73b57a79f2
Valeria <vozzheva@mail.ru>
2022-07-27 21:52:51 +0300

    ref: Section class per instruction

--- src/scripts/components/Section.js
+++ src/scripts/components/Section.js
@@ -1,12 +1,24 @@
+//Первым параметром конструктора принимает объект с двумя свойствами: items и renderer. Свойство items — это массив данных, которые нужно добавить на страницу при инициализации класса. Вы получаете эти данные от Api. Свойство renderer — это функция, которая отвечает за создание и отрисовку данных на странице.
+
+//Второй параметр конструктора — селектор контейнера, в который нужно добавлять созданные элементы.
+
 export default class Section {
-  constructor({ data, renderer }, selector) {
-    this._renderer = renderer; // записываем renderer в this
-    // ...
+  constructor({ data, renderer }, containerSelector) {
+    this._data = data;
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
   }
 
+  //Содержит публичный метод, который отвечает за отрисовку всех элементов. Отрисовка каждого отдельного элемента должна осуществляться функцией renderer.
+
   renderItems() {
-    this._initialArray.forEach((item) => {
+    this._data.forEach((item) => {
       this._renderer(item); // вызываем renderer, передав item
     });
+  }
+
+  //Содержит публичный метод addItem, который принимает DOM-элемент и добавляет его в контейнер.
+  addItem(element) {
+    this._container.prepend(element);
   }
 }
