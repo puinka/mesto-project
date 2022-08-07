9d9ca1a31cd0bc349a17fb0b5d53ee70ada24149
Valeria <vozzheva@mail.ru>
2022-08-07 11:28:36 +0300

    fix: render cards into section in reverse order

--- src/scripts/components/Section.js
+++ src/scripts/components/Section.js
@@ -4,16 +4,13 @@ export default class Section {
     this._container = document.querySelector(containerSelector);
   }
 
-  //Содержит публичный метод, который отвечает за отрисовку всех элементов. Отрисовка каждого отдельного элемента должна осуществляться функцией renderer.
-
   renderItems(items) {
-    items.forEach((item) => {
+    items.reverse().forEach((item) => {
       const element = this._renderer(item);
       this.addItem(element);
     });
   }
 
-  //Содержит публичный метод addItem, который принимает DOM-элемент и добавляет его в контейнер.
   addItem(element) {
     this._container.prepend(element);
   }
